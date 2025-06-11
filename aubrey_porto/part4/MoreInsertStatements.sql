ee0f54613c09bc1f564fab77c7df28d5067f2ad6
oobry <149004178+oobry@users.noreply.github.com>
2025-06-11 00:56:45 +0000

    added a 7 more recipes

---
+++ aubrey_porto/part4/MoreInsertStatements.sql
@@ -0,0 +1,109 @@
+
+/*Oven Baked Buffalo Wings refference recipe: https://www.google.com/url?q=https://www.recipetineats.com/truly-crispy-oven-baked-buffalo-wings-my-wings-cookbook/%23jump-watch&sa=D&source=editors&ust=1749573143672824&usg=AOvVaw3nj6UJdb6C4zVlpR0A6n33*/
+
+INSERT INTO recipe
+VALUES (000005, 'Oven Baked Buffalo Wings', 70, 
+'Crispy Baked Wings
+-Dry overnight (optional) – If you plan ahead, lay the wings on a rack on a rimmed baking tray and let them dry uncovered in the fridge overnight. Alternatively, pat them dry with paper towels.
+-Preheat the oven to 250°F/120°C (all oven types). Put one oven shelf in the lower quarter of the oven and one in the top quarter.
+-Line the tray with foil. Put a rack on the tray then spray the rack with oil.
+-Toss wings – Place the wings in a large bowl. Sprinkle over baking powder and salt. Use hands to toss well (or shake in ziplock bag) to coat wings evenly.
+-Bake 30 minutes on low temp – Place the wings skin side up on the rack (ok if snug, they shrink). Bake on the lower shelf in the oven for 30 minutes.
+-Bake 40 minutes on high – Move the tray up to the higher shelf and turn the oven up to 425°F / 220°C (all oven types). Bake for a further 40 to 50 minutes, rotating the tray halfway through. The wings do not need to be turned over. They are ready when dark golden brown and the skin is very crispy.
+-Toss in sauce – Transfer the wings into a large bowl. Pour over sauce and toss to coat. (Alternatively, serve wings plain with Buffalo Sauce for dipping).
+-Serve immediately with Blue Cheese Dip (for both wings and celery sticks dunking)!
+Buffalo Sauce
+-Whisk together the Sauce ingredients until the sugar is melted. Keep warm or reheat just prior to using.
+Blue Cheese Dip
+-Mash the blue cheese with sour cream until smooth (or to your taste).
+-Add remaining ingredients and mix well until combined, using milk to get it to the consistency you want. Store in the fridge until required. Remove from the fridge 15 minutes before serving.'
+, 8);
+
+/*Creamy tomato beef pasta refference recipe: https://www.google.com/url?q=https://www.recipetineats.com/one-pot-creamy-tomato-beef-pasta/&sa=D&source=editors&ust=1749573143673515&usg=AOvVaw1VkN8R9DUMxHx218zu5Zvm*/
+
+INSERT INTO recipe
+VALUES (000006, 'Creamy tomato beef pasta', 20, 
+'Sauté – Heat the oil on high heat in a large heavy-based pot. Cook garlic and onion for 1 1/2 minutes.
+Cook beef & seasonings – Add beef and cook, breaking it up as you go, until you can no longer see red meat. Add the Italian herbs and cook for 30 seconds, then add tomato paste and cook for 1 minute to cook out the raw flavour.
+All in – Add crushed tomato, chicken stock, salt, pepper and red pepper flakes, if using. Stir, then add the pasta.
+Cook 15 min – Bring the liquid to a simmer, then cook for 15 minutes, stirring every couple of minutes then more frequently towards the end (ensure pasta doesn''t stick to base) until the pasta is just about cooked.
+Creamy – Add cream, then simmer for a further 1 to 2 minutes. It will still be quite saucy – this is what you want! Pasta absorbs liquid quickly, so it will still be nice and oozy when you start eating.
+Serve – Remove from the stove. Give it a good stir then ladle into bowls. Serve with parmesan and parsley.'
+, 4);
+
+/*Tomahawk Steak refference recipe: https://www.google.com/url?q=https://www.recipetineats.com/tomahawk-steak/&sa=D&source=editors&ust=1749573143673903&usg=AOvVaw3GR8voviulV95B9_2BnfbC*/
+
+INSERT INTO recipe
+VALUES (000007, 'Tomahawk Steak', 16, 
+'Oven slow-roast:
+De-chill – Take the steak out of the fridge 1 hour prior to cooking. Remove from packaging and pat the surface dry. (Do not salt).
+Preheat the oven to 140°C/285°F (120°C fan-forced).
+Season – Sprinkle the steak meat with salt and pepper, then use your hands to rub it in.
+Slow(ish) roast – Put the steak on a rack and set it on a baking tray. (Note 2) Bake for 40 to 45 minutes, or until the internal temperature is 50°C/122°F (for medium rare, see Note 3 for other doneness).
+Rest – Take the tray out of the oven and leave the steak on the counter for 10 minutes. (Internal temperature should rise by 3 degrees).
+Sear:
+Preheat the BBQ on high until it''s screaming hot. Use the flat plate side if you''re not a confident BBQ''er, or the grill side if you''re a BBQ master. (Note 4)
+Oil – Drizzle the oil on the steak and lightly pat to spread (don''t rub off the salt), using most of the surface area of the meaty part.
+Turn BBQ DOWN – If using the grill side, reduce to low (if yours is strong, mine is) or medium low (for weaker ones). If using the flat plate side, lower to medium. Do this just before putting the tomahawk on.
+Edges first – Using tongs, hold the steak upright and sear the edges and bone first (only if you''ve got meat on it), rotating and moving every minute or so to achieve this all the way around.
+⚠️Flare-ups – If searing on the grill side, don’t walk away – flare-ups are inevitable. See Note 4 for managing.
+Steak crust – Then place the steak down on the grill and cook for 3 to 4 minutes on each side, or until the internal temperature is 55°C / 131°F. For classic X grill lines, rotate once 45° partway through.
+Rest the steak on a rack for 3 minutes – the internal temperature will rise to 57°C/135°F which is perfect medium rare.
+Serve – Cut into 1 cm / 0.4" thick slices. Serve with steak sauce of choice! (Pictured: Cowboy Butter, highly recommend!).'
+, 2);
+
+/*Harvest root soup refference recipe: https://www.google.com/url?q=https://www.recipetineats.com/country-harvest-root-vegetable-soup/&sa=D&source=editors&ust=1749573143674336&usg=AOvVaw0pL_0SaIxllm-C3cKswQs8*/
+
+INSERT INTO recipe
+VALUES (000008, 'Harvest root soup', 50, 
+'Sauté – Put the olive oil and butter in a large heavy-based pot over medium-high heat. Once the butter is melted, cook the onion and garlic for 2 minutes until the onion is softened.
+Add root vegetables, thyme and curry powder. Cook for three minutes stirring regularly, until the outside of the vegetables starts to soften.
+Simmer 15 minutes – Turn stove up to high. Add water, salt and pepper. Stir, then once it comes to a simmer, lower heat to medium high and simmer rapidly for 15 minutes (no lid) until all the vegetables are soft (check with knife).
+Stir in cream, simmer for 1 minute.
+Blitz – Remove from heat and use a stick blender to blitz until smooth. (Note 5 for blender) Adjust to taste – water to thin, salt and pepper if needed, extra cream for more indulgent.
+Serve in bowls. Drizzle with cream, pinch of curry powder, parsley and pepper. Serve with warm crusty bread!'
+, 5);
+
+/*Beef Chow Mein refference recipe: https://www.google.com/url?q=https://www.recipetineats.com/beef-chow-mein-noodles/&sa=D&source=editors&ust=1749573143674733&usg=AOvVaw0BHwOmuPrN2sbvtt5u868l*/
+
+INSERT INTO recipe
+VALUES (000009, 'Beef Chow Mein', 10, 
+'Sauce – Mix ingredients in a small bowl then set aside. Some is used to flavour the beef, then the rest for the noodles.
+Sauce thickener (cornflour slurry) – Mix the cornflour / cornstarch and water in a separate small bowl.
+Prepare noodles per packet directions then drain.
+Cook beef – Heat the oil in a large non-stick pan over high heat. Cook garlic for 10 seconds, then add the beef and cook until you can no longer see pink. Add 1 1/2 tablespoons of the Sauce and the white part of the green onions. Cook for another 2 minutes to get the beef nicely caramelised.
+Finish sauce – Mix the cornflour slurry into the remaining sauce.
+Egg – Add the egg into the pan then mix it through the beef, it will sort of scramble. Egg makes the beef stick to the noodles better!
+Cabbage & carrot – When the egg is mostly cooked, add the carrot and cabbage. Cook for 1 minute until cabbage starts to wilt.
+Sauce & noodles – Give the sauce a quick mix. Add the bean sprouts, noodles and sauce into the pan. Toss well for 1 minute or until sauce is dispersed through the noodles. Add green part of green onions. Toss for another 1 minute.
+Serve – Divide between bowls and serve!'
+, 2);
+
+/*Really Quick Broccoli Pasta refference recipe: https://www.google.com/url?q=https://www.recipetineats.com/quick-broccoli-pasta/&sa=D&source=editors&ust=1749573143675695&usg=AOvVaw1g_7fngJ8qeGNNRXls7SWn*/
+
+INSERT INTO recipe
+VALUES (000010, 'Really Quick Broccoli Pasta', 15, 
+'Cook pasta: Boil a large pot of water with 2 tsp salt, add pasta.
+While pasta is cooking:
+Chop broccoli: Chop broccoli into small florets.
+Cook broccoli: Add broccoli into water 1 – 2 minutes before pasta is cooked.
+Sauce ingredients in jar: Place Sauce ingredients in a jar with lid.
+Reserve pasta water: SCOOP OUT 1 cup pasta cooking water just before draining. Then drain and return pasta back into same pot on turned off stove.
+Add pasta water into Sauce: Add 1/2 cup pasta water to Pasta Sauce jar, shake well.
+Add Sauce & Cheese to pasta: Pour Sauce and add cheese into pot with pasta.
+Stir then serve! Stir vigorously, adding more pasta water if required. Add more salt and pepper if required. Serve immediately, garnished with parmesan.'
+, 4);
+
+/*Vietnamese Lettuce Wraps refference recipe: https://www.recipetineats.com/vietnamese-lettuce-wraps-with-peanut-sauce/#wprm-recipe-container-128663*/
+
+INSERT INTO recipe
+VALUES (000011, 'Vietnamese Lettuce Wraps', 20, 
+'Pickle first – Put the boiling water, salt and sugar in a bowl. Mix to to dissolve, then add vinegar. Submerge vegetables and leave for 2 hours. Drain then use per recipe (or store in fridge submerged in liquid).
+Peanut sauce – Mix the peanut sauce ingredients and set aside for the flavours to meld while you prepare the platter. Start with 1 tbsp lime juice and add more if you want tarter.
+Prawns (shrimp) – Cut the prawns in half horizontally and remove the vein.
+Noodles – Soak the noodles in boiling water per packet directions. Rinse under cold water (to prevent stickiness) then drain very well.
+Lay it all out – Pile everything onto a giant platter or put things in little bowls, then let everybody help themselves!
+Assemble – Here''s my order: lettuce leaf, noodles first, then veg* (carrots, bean sprouts etc), prawns, stuff herbs down the side, drizzle with sauce, sprinkle with peanuts and chilli. Bundle and bite!'
+, 4);
+
+
